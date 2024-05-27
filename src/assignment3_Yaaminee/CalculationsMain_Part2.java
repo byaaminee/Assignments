3a05516652310b7047c42d8d3196c59aa7b5094e
byaaminee <byaaminee@gmail.com>
2024-05-26 23:23:17 -0400

    Pushing updated file to branch

--- src/assignment3_Yaaminee/CalculationsMain_Part2.java
+++ src/assignment3_Yaaminee/CalculationsMain_Part2.java
@@ -13,7 +13,7 @@ public class CalculationsMain_Part2 {
 		System.out.println("Sum of 2 integer values: " + cal.sum(40, 50) );
 		System.out.println("Sum of 3 integer values: " + cal.sum(10, 20, 30) );
 		System.out.println("Sum of 2 double values: " + cal.sum(10d, 23.6d) );
-		System.out.println("Sum of 3 double values: " + cal.sum(1000d, 5.2d) );
+		System.out.println("Sum of 3 double values: " + cal.sum(1000d, 5.2d, 20d) );
 
 	}
 
